6eaada8957d121bf9e1c6ac4e6ec4e4a16cbbe30
Markiian Benovskyi <MarkusBansky@users.noreply.github.com>
2023-04-21 21:29:35 +0100

    Configured model command and config

--- background.js
+++ background.js
@@ -1,3 +1,3 @@
 const shell = require('shelljs');
 
-shell.exec('libcamera-detect -t 0 -o ./public/images/bird-%06d.jpg --lores-width 400 --lores-height 300 --post-process-file object_detect_tf.json --object bird');
+shell.exec('libcamera-detect -t 0 -o ./public/images/bird-%06d.jpg --lores-width 800 --lores-height 600 --post-process-file object_detect_tf.json --object bird');
